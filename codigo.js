50152e3f99082021a9464254ead81493b18bf179
LeandroSpohr <leandro.spohr@acertapromotora.com.br>
2022-04-04 22:31:00 -0300

    add jokenpo

--- codigo.js
+++ codigo.js
@@ -9,3 +9,27 @@ function soma() {
   resultado.value = soma;
 
 }
+
+function jogar(value) {
+  var qtdJogos = "JOGOS";
+  var vitorias = "VITORIAS";
+  var derrotas = "DERROTAS";
+  var total = +localStorage.getItem(qtdJogos) + 1;
+  localStorage.setItem(qtdJogos, total);
+
+  var escolhaPC = Math.floor(Math.random() * (3 - 1) + 1);
+
+  if (escolhaPC === value) {
+    alert("empate");
+  }
+  total();
+}
+
+function total() {
+  var qtdJogos = "JOGOS";
+
+  var total = localStorage.getItem(qtdJogos);
+  
+  var inputTotal = document.getElementById("total");
+  inputTotal.value = total;
+}
